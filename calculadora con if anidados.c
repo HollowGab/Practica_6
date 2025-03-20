8be8cad8641f22d6301454888e0c643e0176664a
HollowGab <gabri.areani411@gmail.com>
2025-03-20 14:49:09 -0600

    Update and rename calculadora.c to calculadora con if anidados.c

---
+++ calculadora con if anidados.c
@@ -0,0 +1,25 @@
+// Arellanes Aniceto Gabriel
+// ejercicio3
+#include <stdio.h>
+
+int main() {
+    int  a, b, res;
+    int op;
+    
+    printf("Menu\n 0. Salir\n 1. Suma\n 2. Resta\n 3. Multiplicacion\n 4. Division");
+    scanf (%i,&op);
+    if (op==1) {
+        printf("SUMA\n");
+        printf("Ingrese el primer numero \n");
+        scanf(%i,&a);
+        printf("Ingrese el segundo numero \n");
+        scanf(%i,&a);
+        res= a+b;
+        
+        
+    } else {
+        
+    }
+    
+    return 0;
+}
